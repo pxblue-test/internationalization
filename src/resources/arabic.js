04d78bb0a46b433b5239f5996738fe1420aaefa6
joebochill <boyle.p.joseph@gmail.com>
2019-06-28 22:17:50 -0400

    Move react to orphan branch

---
+++ src/resources/arabic.js
@@ -0,0 +1,8 @@
+export const MENU_ITEMS = ['الرئيسية', 'مشاريع', 'إنذار', 'إعدادات', 'مساعدة']
+export const FRUIT_LIST = ['تفاحة', 'موز', 'البرتقالي', 'جوافة', ' جوزة الهند', 'عنب', 'ليمون', 'أناناس', 'البطيخ']
+export const DROPDOWN_LANGS = ['الإنجليزية', 'الأسبانية', 'ألمانية', 'عربى']
+export const ADD_TO_CART = 'أضف إلى العربة';
+export const MORE_INFO = 'مزيد من المعلومات';
+export const ITEMS = 'عناصر';
+export const I18N = 'تدويل'
+export const BLUE = 'أزرق'
---
+++ src/resources/english.js
@@ -0,0 +1,8 @@
+export const MENU_ITEMS = ['Home', 'Projects', 'Warnings', 'Settings', 'Help']
+export const FRUIT_LIST = ['Apple', 'Banana', 'Grape', 'Orange', 'Guava', 'Coconut', 'Lemon', 'Pineapple', 'Watermelon']
+export const DROPDOWN_LANGS = ['English', 'Spanish', 'German', 'Arabic']
+export const ADD_TO_CART = 'Add to Cart'
+export const MORE_INFO = 'More information'
+export const ITEMS = 'Items'
+export const I18N = 'Internationalization'
+export const BLUE = 'Blue'
